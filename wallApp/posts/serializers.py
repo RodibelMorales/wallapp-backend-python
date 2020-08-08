8da5eaea6174d136529bc261492376eaa9691af4
RodibelM <rodibelmorales1993@gmail.com>
2020-08-07 20:21:15 -0500

    nested relations to post owner fixed

--- wallApp/posts/serializers.py
+++ wallApp/posts/serializers.py
@@ -3,7 +3,7 @@ from wallApp.models import Post
 from wallApp.models import Comment
 from wallApp.models import User
 from drf_extra_fields.relations import PresentablePrimaryKeyRelatedField
-
+from datetime import date
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -12,8 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     user = PresentablePrimaryKeyRelatedField(
         queryset=User.objects.all(),
-        presentation_serializer=UserSerializer,
-        
+        presentation_serializer=UserSerializer
     )
     class Meta:
         model = Comment
@@ -21,11 +20,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
-    user = PresentablePrimaryKeyRelatedField(
-        queryset=User.objects.all(),
-        presentation_serializer=UserSerializer,
-        
-    )
+    post_owner=UserSerializer(source='user',read_only=True)
     class Meta:
         model=Post
-        fields=['id','content','likes','created_at','updated_at','deleted_at','privacity_id','comments','user']
+        fields=['id','content','likes','created_at','updated_at','deleted_at','privacity_id','user_id','comments','post_owner']
+
